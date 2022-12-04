21b63a6ac5adb17b649f96e7439d7a1a519853c6
Danyélica <danydayane67@gmail.com>
2022-12-04 16:49:40 -0300

    some changes made

--- collage-generator/pages/sample1/example.jsx
+++ collage-generator/pages/sample1/example.jsx
@@ -7,68 +7,66 @@ import styles from "../../styles/Sample1.module.css";
 
 export default function Sample1() {
   return (
-    <div className={styles.container}>
+    <div className='container'>
       <Head>
         <title>Exemplo 1</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.firstExample}>
-        <div className='mainPicture'>
-          <div className={styles.texture}>
-            <div className={styles.top}>
-              <div className={styles.lineRight}></div>
-              <p className={styles.topTitle}>small • cute • friendly </p>
-              <div className={styles.lineLeft}></div>
-            </div>
-            <h1 className={styles.mainTitle}>Nori</h1>
+      <main className='mainPicture'>
+        <div className={styles.texture}>
+          <div className={styles.top}>
+            <div className={styles.lineRight}></div>
+            <p className={styles.topTitle}>small • cute • friendly </p>
+            <div className={styles.lineLeft}></div>
+          </div>
+          <h1 className={styles.mainTitle}>Nori</h1>
+          <Image
+            src={PhotosFirstSample.Photo1}
+            className={styles.firstImage}
+            alt='A white cat looking at the camera'
+          />
+          <Image
+            src={CloseIcon}
+            className={styles.closeIcon}
+            alt='A white cat looking at the camera'
+          />
+          <Image
+            src={SearchBar}
+            className={styles.searchBar}
+            alt='A white cat looking at the camera'
+          />
+          <div className={styles.rowFirst}>
             <Image
-              src={PhotosFirstSample.Photo1}
-              className={styles.firstImage}
-              alt='A white cat looking at the camera'
+              src={PhotosFirstSample.Photo2}
+              alt='A white cat sleeping all stretch'
+              className={styles.tinyImage}
             />
+            <h3 className={styles.texts}>kitty</h3>
+          </div>
+          <div className={styles.row}>
+            <h3 className={styles.texts}>baby</h3>
             <Image
-              src={CloseIcon}
-              className={styles.closeIcon}
-              alt='A white cat looking at the camera'
+              src={PhotosFirstSample.Photo3}
+              alt='A white cat sleeping all stretch'
+              className={styles.tinyImage}
             />
+          </div>
+          <div className={styles.row}>
             <Image
-              src={SearchBar}
-              className={styles.searchBar}
-              alt='A white cat looking at the camera'
+              src={PhotosFirstSample.Photo4}
+              alt='A white cat sleeping all stretch'
+              className={styles.tinyImage}
             />
-            <div className={styles.rowFirst}>
-              <Image
-                src={PhotosFirstSample.Photo2}
-                alt='A white cat sleeping all stretch'
-                className={styles.tinyImage}
-              />
-              <h3 className={styles.texts}>kitty</h3>
-            </div>
-            <div className={styles.row}>
-              <h3 className={styles.texts}>baby</h3>
-              <Image
-                src={PhotosFirstSample.Photo3}
-                alt='A white cat sleeping all stretch'
-                className={styles.tinyImage}
-              />
-            </div>
-            <div className={styles.row}>
-              <Image
-                src={PhotosFirstSample.Photo4}
-                alt='A white cat sleeping all stretch'
-                className={styles.tinyImage}
-              />
-              <h3 className={styles.texts}>tiny</h3>
-            </div>
-            <div className={styles.row}>
-              <h3 className={styles.texts}>happy</h3>
-              <Image
-                src={PhotosFirstSample.Photo5}
-                alt='A white cat sleeping all stretch'
-                className={styles.tinyImage}
-              />
-            </div>
+            <h3 className={styles.texts}>tiny</h3>
+          </div>
+          <div className={styles.row}>
+            <h3 className={styles.texts}>happy</h3>
+            <Image
+              src={PhotosFirstSample.Photo5}
+              alt='A white cat sleeping all stretch'
+              className={styles.tinyImage}
+            />
           </div>
         </div>
       </main>
--- collage-generator/pages/sample2/example.jsx
+++ collage-generator/pages/sample2/example.jsx
@@ -3,13 +3,13 @@ import styles from "../../styles/Sample2.module.css";
 
 export default function Sample2() {
   return (
-    <div className={styles.container}>
+    <div className='container'>
       <Head>
         <title>Exemplo 2</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.secondExample}>
+      <main className={styles.main}>
         <p>opa</p>
       </main>
     </div>

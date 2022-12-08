1b7f9e37aabdfb92da41004f0561f36a34b7969e
Danyélica <danydayane67@gmail.com>
2022-12-08 17:04:37 -0300

    the generated page is functioning

--- collage-generator/pages/sample1/done.jsx
+++ collage-generator/pages/sample1/done.jsx
@@ -1,85 +1,121 @@
-import Head from "next/head";
 import Image from "next/image";
-import { PhotosFirstSample } from "../api/photos";
+import { useEffect, useState } from "react";
+import { useLocalStorage } from "react-use";
+import Info from "../../components/info";
 import CloseIcon from "../../public/assets/images/example1/close-icon.png";
 import SearchBar from "../../public/assets/images/example1/search-bar.png";
 import styles from "../../styles/Sample1.module.css";
+import { uploadSite } from "../../utils/requests";
 
 export default function Sample1() {
+  const [done, setDone] = useState(false);
+  const localInfo = useLocalStorage("info");
+  const [info, setInfo] = useState(localInfo[0]);
+
+  useEffect(() => {
+    if (info) {
+      handleFormatting();
+    }
+  }, []);
+
+  function handleFormatting() {
+    const formattingTop = info.top.split(",");
+    const formattedTop = formattingTop.join(" • ");
+
+    const formattedWords = info.words.split(",");
+
+    setInfo({ ...info, top: formattedTop, words: formattedWords });
+    setDone(true);
+  }
+
+  async function handleSavingImage() {
+    const body = {
+      url: "https://collage-generator.vercel.app/sample1/done",
+      full_screen: true,
+      google_fonts: "Cookie|Great Vibes|Bebas Neue",
+    };
+
+    const response = await uploadSite(body);
+
+    return window.location.href(response.url);
+  }
+
   return (
-    <div className='container'>
-      <Head>
-        <title>Exemplo 1</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <div>
+      <header>
+        <Info />
+      </header>
 
-      <main className='mainPicture'>
-        <div className={styles.texture}>
-          <div className={styles.top}>
-            <div className={styles.lineRight}></div>
-            <p className={styles.topTitle}>small • cute • friendly </p>
-            <div className={styles.lineLeft}></div>
-          </div>
-          <h1 className={styles.mainTitle}>Nori</h1>
-          <Image
-            src={PhotosFirstSample.Photo1}
-            className={styles.firstImage}
-            alt='A white cat looking at the camera'
-          />
-          <Image
-            src={CloseIcon}
-            className={styles.closeIcon}
-            alt='A white cat looking at the camera'
-          />
-          <Image
-            src={SearchBar}
-            className={styles.searchBar}
-            alt='A white cat looking at the camera'
-          />
-          <div className={styles.rowFirst}>
-            <Image
-              src={PhotosFirstSample.Photo2}
-              alt='A white cat sleeping all stretch'
-              className={styles.tinyImage}
+      <div className='container'>
+        {done ? (
+          <main className={styles.main}>
+            <div className='mainPicture'>
+              <div className={styles.texture}>
+                <div className={styles.top}>
+                  <div className={styles.lineRight}></div>
+                  <p className={styles.topTitle}>{info.top} </p>
+                  <div className={styles.lineLeft}></div>
+                </div>
+                <h1 className={styles.mainTitle}>{info.mainText}</h1>
+                <img src={info.squarePic} className={styles.firstImage} />
+                <Image
+                  src={CloseIcon}
+                  className={styles.closeIcon}
+                  alt='Um ícone de fechar'
+                />
+                <Image
+                  src={SearchBar}
+                  className={styles.searchBar}
+                  alt='Uma barra de busca'
+                />
+                <div className={styles.rowFirst}>
+                  <img src={info.firstRecPic} className={styles.tinyImage} />
+                  <h3 className={styles.texts}>{info.words[0]}</h3>
+                </div>
+                <div className={styles.row}>
+                  <h3 className={styles.texts}>{info.words[1]}</h3>
+                  <img src={info.secondRecPic} className={styles.tinyImage} />
+                </div>
+                <div className={styles.row}>
+                  <img src={info.thirdRecPic} className={styles.tinyImage} />
+                  <h3 className={styles.texts}>{info.words[2]}</h3>
+                </div>
+                <div className={styles.row}>
+                  <h3 className={styles.texts}>{info.words[3]}</h3>
+                  <img src={info.fourthRecPic} className={styles.tinyImage} />
+                </div>
+              </div>
+            </div>
+            <button
+              onClick={() => handleSavingImage()}
+              className={styles.button}
+            >
+              Salvar minha imagem
+            </button>
+          </main>
+        ) : (
+          <div className={styles.center}>
+            <img src='https://media.tenor.com/wHdr_idzE20AAAAi/black-cat.gif' />
+            <h1 className={styles.waitingTitle}>
+              Fazendo o meu melhor e procurando a sua foto
+            </h1>
+            <img
+              className={styles.loading}
+              src='https://media.tenor.com/5o2p0tH5LFQAAAAj/hug.gif'
             />
-            <h3 className={styles.texts}>kitty</h3>
           </div>
-          <div className={styles.row}>
-            <h3 className={styles.texts}>baby</h3>
-            <Image
-              src={PhotosFirstSample.Photo3}
-              alt='A white cat sleeping all stretch'
-              className={styles.tinyImage}
-            />
-          </div>
-          <div className={styles.row}>
-            <Image
-              src={PhotosFirstSample.Photo4}
-              alt='A white cat sleeping all stretch'
-              className={styles.tinyImage}
-            />
-            <h3 className={styles.texts}>tiny</h3>
-          </div>
-          <div className={styles.row}>
-            <h3 className={styles.texts}>happy</h3>
-            <Image
-              src={PhotosFirstSample.Photo5}
-              alt='A white cat sleeping all stretch'
-              className={styles.tinyImage}
-            />
-          </div>
-        </div>
-      </main>
-      <style jsx>{`
-        .mainPicture {
-          height: 1080px;
-          width: 608px;
+        )}
+        <style jsx>{`
+          .mainPicture {
+            height: 1080px;
+            width: 608px;
 
-          background-image: url(${PhotosFirstSample.Photo6.src});
-          background-position: center;
-          background-size: cover;
-        }
-      `}</style>
+            background-image: url(${info && info.backgroundPic});
+            background-position: center;
+            background-size: cover;
+          }
+        `}</style>
+      </div>
     </div>
   );
 }
--- collage-generator/utils/requests.js
+++ collage-generator/utils/requests.js
@@ -1,12 +1,23 @@
-import api from "../pages/api/api";
+import api from "../pages/api/mainApi";
+import secondApi from "../pages/api/secondApi";
 
-export default async function uploadFiles(formData) {
+export async function uploadFiles(formData) {
   try {
     const { data } = await api.post("/upload-multiple", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function uploadSite(body) {
+  try {
+    const { data } = await secondApi.post("/", body);
 
     return data;
   } catch (error) {

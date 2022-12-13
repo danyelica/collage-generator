c519ca9fbdd7fda30b0b5dca43b01575dc19e710
Danyélica <danydayane67@gmail.com>
2022-12-13 16:23:56 -0300

    the api timeout increased

--- collage-generator/pages/api/mainApi.js
+++ collage-generator/pages/api/mainApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://collage-generator-api.up.railway.app",
-  timeout: 10000,
+  timeout: 12000,
   headers: {
     "Content-Type": "application/json",
   },

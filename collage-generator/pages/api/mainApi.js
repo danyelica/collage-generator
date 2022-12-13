95f7457a7ae14eb84d9f7200bb94ef2c7011a12e
Danyélica <danydayane67@gmail.com>
2022-12-13 16:34:02 -0300

    trying to figure this out, is taking too long to post something

--- collage-generator/pages/api/mainApi.js
+++ collage-generator/pages/api/mainApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://collage-generator-api.up.railway.app",
-  timeout: 12000,
+  timeout: 20000,
   headers: {
     "Content-Type": "application/json",
   },
